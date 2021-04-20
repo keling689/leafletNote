a9d319116c26c919e915dfdc51e96fffea0adbe2
chenbin <chenbin449@qq.com>
2021-04-20 14:39:18 +0800

    创建项目

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // // 基本路径
+  // publicPath: './',
+  // // 输出文件目录
+  // outputDir: '../cordova-vue/www',
+  // productionSourceMap: false, // 不生成map
+  // assetsDir: 'assets', // 静态资源目录（js, css, img）
+  // lintOnSave: true, // 是否开启eslint
+  // devServer: {
+  //   open: true, // 是否自动弹出浏览器页面
+  //   host: 'localhost',
+  //   port: '8081',
+  //   https: false, // 是否使用https协议
+  //   hotOnly: false, // 是否开启热更新
+  //   proxy: {
+  //     '/api': {
+  //       // target: 'http://10.0.0.170:9900', // API服务器的地址
+  //       target: 'http://10.0.0.170:19900', // API服务器的地址
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  // }
+}
